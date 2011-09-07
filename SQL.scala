36ceb04f69fb3d45a3aeca7bc6296fdfac1db5c7
Ali Kaafarani <kaafarani.ali@gmail.com>
2011-09-07 23:00:29 +0200

    Add initial SQL object

---
+++ SQL.scala
@@ -0,0 +1,34 @@
+import java.sql._
+
+case class Row(id: Long, values: List[AnyRef])
+
+object SQL {
+
+  var connection: Option[Connection] = None
+
+  @throws(classOf[ClassNotFoundException])
+  @throws(classOf[SQLException])
+  def connect(driver: String, jdbcURL: String) = {
+    Class.forName("org.sqlite.JDBC")
+    connection = Some(DriverManager.getConnection(jdbcURL))
+  }
+
+  def select(table: String) = {
+    // todo - sanitize table String - SQL injection
+    if(connection isEmpty) {
+      throw new Exception("TODO some better exception")
+    }
+    val statement = connection.get.prepareStatement("select * from `" + table + "`")
+    statement.execute
+    val resultset = statement.getResultSet()
+    println(resultset.getMetaData().getColumnCount())
+    var rows = List[Row]()
+    while(resultset.next()) {
+      val values = for(i <- 2 to resultset.getMetaData().getColumnCount())
+        yield resultset.getObject(i).asInstanceOf[AnyRef]
+      rows = Row(resultset.getObject(1).asInstanceOf[Int].toLong, values.toList) :: rows
+    }
+    rows
+  }
+
+}
