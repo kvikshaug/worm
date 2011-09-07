19ea24c6aa6cf3cf38a04b7690ef331e5a782cb7
Ali Kaafarani <kaafarani.ali@gmail.com>
2011-09-07 15:13:24 +0200

    Keep class type in private variable

--- Reflection.scala
+++ Reflection.scala
@@ -7,8 +7,9 @@ object ORM {
 }
 
 class ORM {
+  private val c = this.getClass
   private var id: Option[Long] = None
-  private def fields = this.getClass.getDeclaredFields.map(f => retrieveField(f)).flatten.toList
+  private def fields = c.getDeclaredFields.map(f => retrieveField(f)).flatten.toList
 
   @throws(classOf[IllegalStateException])
   def insert() = {
@@ -18,7 +19,7 @@ class ORM {
     }
     val fields = this.fields
     println(String.format("insert into `%s` (%s) values (%s)",
-            this.getClass.getSimpleName,
+            c.getSimpleName,
             commaize(fields.map(_.name)),
             commaize(fields.map(_.value))))
   }
@@ -30,7 +31,7 @@ class ORM {
     }
     val fields = this.fields
     println(String.format("update `%s` set (%s) where id=`" + id.get + "`",
-            this.getClass.getSimpleName,
+            c.getSimpleName,
             fields.map(f => f.name + "=" + f.value + ", ")))
   }
 
@@ -41,13 +42,13 @@ class ORM {
   def retrieveField(field: JVMField): Option[Field] = {
     def asGetter(s: String) = "get" + s(0).toUpper + s.tail
     // check for getter
-    val getter = this.getClass.getMethods.find(_.getName == asGetter(field.getName))
+    val getter = c.getMethods.find(_.getName == asGetter(field.getName))
     if(getter isDefined) {
       return Some(Field(field.getName, getter.get.invoke(this).toString))
     }
 
     // check for method with same name
-    val method = this.getClass.getMethods.find(_.getName == field.getName)
+    val method = c.getMethods.find(_.getName == field.getName)
     if(method isDefined) {
       return Some(Field(field.getName, method.get.invoke(this).toString))
     }
