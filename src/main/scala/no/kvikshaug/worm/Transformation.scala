964b504d3042c277eeb82f41c30fc5b73d9f5d5a
Ali Kaafarani <kaafarani.ali@gmail.com>
2011-09-28 14:08:44 +0200

    Add transformation class
    For seperation of business logic

---
+++ src/main/scala/no/kvikshaug/worm/Transformation.scala
@@ -0,0 +1,35 @@
+package no.kvikshaug.worm
+
+import java.lang.reflect.{Field => JVMField}
+//import java.lang.reflect.Field
+
+class Attribute
+case class PrimaryKey() extends Attribute
+case class ForeignKeyNew() extends Attribute // Refactor to ForeignKey when that class is removed
+case class Primitive() extends Attribute
+
+case class Table(name: String, rows: List[Row], obj: Worm)
+case class Row(name: String, value: Option[AnyRef], attribute: Attribute = Primitive())
+
+object Transformation {
+
+  /* Does not verify that a connection to SQL has been
+     performed, so do that before calling this method */
+  def objectToSql(obj: Worm): Table = {
+    // Traverse all the fields of the class
+    val rows = obj.getClass.getDeclaredFields.map { f =>
+      f.setAccessible(true)
+      if(classOf[Worm].isAssignableFrom(f.getType)) {
+        // It's another custom class that extends Worm
+        Row(f.getName, Some(objectToSql(f.get(obj).asInstanceOf[Worm])), ForeignKeyNew())
+      //} else if() {
+        // It's a collection
+        
+      } else {
+        // It's something else, assume primitive
+        Row(f.getName, Some(f.get(obj)))
+      }
+    }.toList
+    Table(obj.getClass.getSimpleName, rows, obj)
+  }
+}
